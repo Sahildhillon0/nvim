1a13303a1d4613fe6a6d101d0baebdfc38a87218
Sahildhillon0 <sahildhillon609@gmail.com>
2025-02-06 19:40:25 +0530

    adding 'search from dict.'

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,5 @@ vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { no
 vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('n', '<leader>fd', '<cmd>Telescope find_files cwd=/<CR>', { noremap = true, silent = true })
